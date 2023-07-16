e440cdcf43135def803d6b064c5db8d136e91f62
ssy-lbh <59327856+ssy-lbh@users.noreply.github.com>
2023-07-16 21:29:56 +0800

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Code/C++/MathLib
-# Build directory: D:/Code/C++/MathLib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("tests")
